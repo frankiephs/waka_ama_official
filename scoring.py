3a7562d46c6e8c9b459ecb7de4fd8829abf9e659
dadpenguin <franklinbula.100@gmail.com>
2024-06-30 15:33:19 +1200

    added return scores of the file

--- scoring.py
+++ scoring.py
@@ -6,26 +6,52 @@ class scoring_c():
         pass
 
     
-    def return_scores(filename,file_contents): # file_contents is a list
-        
-        """# returns the scores in a dictionary. Includes the filename {filename:{reg1:100,reg2:200}}
-        file_headers = file_contents[0] # first
-        file_body = file_contents[1:-1] # second to the last
-        
-        # 2,57145,3,HEKE  Kura,,Kaihoe o Ngati Rehia Trust,3:10.92,,4.28,,,16:14:09.09,,,,4.28,4.28\n
+    def return_scores(filename,formatted_file_contents,points_refference): # file_contents is a list
+        # returns the regional association scores with filename
+        """
+        {
+        "1":8,
+        "2":7,
+        "3":6,
+        "4":5,
+        "5":4,
+        "6":3,
+        "7":2,
+        "8":1,
+        ">":1,
 
-        # Convert to list and remove the additional commas
-        for team in file_body:
-            team_split = team.split(',')
-            for i in team_split:
-                if i == "":
-                    team_split.remove(i or "")
-                    team_place = team_split[0]
-                    team_lane = team_split[3]
-                    team_name = team_split[4]
-                    team_regional_association = team_split[5]
-"""
+        }
+        """
+        
+        if points_refference.keys()[-1] == ">":
+            last_option = True
+            last_option_value = points_refference.values()[-1]
+            points_refference.pop() # removes the 'last option'
+        
+        regional_association_scores = {}
+
+        teams_list = list(formatted_file_contents.values())[1] # gets the teams list
+        last_points_refference = points_refference.keys()[-1]
+        for team_dict in teams_list:
+            team_place = team_dict["place"]
+            team_regional_association = team_dict["regional_association"]
+            for place_refference in points_refference.keys():
+                if place_refference == team_place:
                     
+                    if team_regional_association in regional_association_scores:
+                        regional_association_scores[team_regional_association] += points_refference[place_refference]
+                    else:
+                        regional_association_scores[team_regional_association] = points_refference[place_refference]
+
+                elif last_option == True and team_place > last_points_refference:
+                    if team_regional_association in regional_association_scores:
+                        regional_association_scores[team_regional_association] += last_option_value
+                    else:
+                        regional_association_scores[team_regional_association] = last_option_value
+
+        file_scores = {filename:regional_association_scores}
+        return file_scores
+        
         
         
         
