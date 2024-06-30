232cb78682f44650c2a3d0a95dc75d1779a0ddb4
dadpenguin <franklinbula.100@gmail.com>
2024-06-30 12:20:43 +1200

    find specific keyword only

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ print(fr.file_read_c.return_years(parent_path))
 ask_year = int(input("Type your year >> "))
 target_year = fr.file_read_c.find_year_path(parent_path,ask_year,"WakaNats")
 files_list = fr.file_read_c.return_files(target_year)
+
+# find specific keyword only
+ask_keyword = input("filter keyword >> ")
+filtered_files_list = []
+for file in files_list:
+    if ask_keyword in file.lower():
+        filtered_files_list.append(file)
+    else:
+        continue
+print(len(filtered_files_list), ask_keyword, "found")
+
+
+
